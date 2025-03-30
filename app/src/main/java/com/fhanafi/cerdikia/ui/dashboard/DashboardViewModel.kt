b77a17cfa174c59dc87b387cfc09ec9b8023f435
Fhanafii <fhanafi544@gmail.com>
2025-03-30 20:27:03 +0700

    inital Project

---
+++ app/src/main/java/com/fhanafi/cerdikia/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.fhanafi.cerdikia.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
