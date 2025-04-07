c72e792120eb3bdef035cabd85bd98922cf8d522
Fhanafii <fhanafi544@gmail.com>
2025-04-07 12:57:14 +0700

    Creating Top bar to show player status using dummy data

---
+++ app/src/main/java/com/fhanafi/cerdikia/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.fhanafi.cerdikia
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainViewModel : ViewModel() {
+    // TODO: this not declare as player flag but it was an english course so in the future i will add some quiz subjects
+    // so i think it has a wrong implementation and i will change it later
+    private val _playerFlag = MutableStateFlow(R.drawable.ic_flag)
+    val playerFlag: StateFlow<Int> = _playerFlag.asStateFlow()
+
+    private val _playerGems = MutableStateFlow(100)
+    val playerGems: StateFlow<Int> = _playerGems.asStateFlow()
+
+    private val _playerEnergy = MutableStateFlow(5)
+    val playerEnergy: StateFlow<Int> = _playerEnergy.asStateFlow()
+
+
+    //Simulasi API untuk mendapatkan data pemain (change it later when the api was ready)
+    fun updatePlayerStatus(newGems: Int, newEnergy: Int, newFlag: Int = _playerFlag.value){
+        _playerFlag.value = newFlag
+        _playerGems.value = newGems
+        _playerEnergy.value = newEnergy
+    }
+}
---
+++ app/src/main/java/com/fhanafi/cerdikia/ui/components/PlayerStatusBar.kt
@@ -0,0 +1,101 @@
+package com.fhanafi.cerdikia.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.fhanafi.cerdikia.R
+import kotlinx.coroutines.flow.StateFlow
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PlayerStatusBar(
+    flagResourceId: StateFlow<Int>, // Use StateFlow for dynamic updates
+    gemImageResourceId: Int,
+    gemCount: StateFlow<Int>, // Use StateFlow for dynamic updates
+    energyImageResourceId: Int,
+    energyCount: StateFlow<Int>, // Use StateFlow for dynamic updates
+    bottomBorderColor: Color = Color(38, 137,181),
+    bottomBorderSize: Dp = 1.5.dp
+) {
+    TopAppBar(
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                // Flag
+                val currentFlag by flagResourceId.collectAsState(initial = R.drawable.ic_flag) // Provide a default subject as english course
+                Image(
+                    painter = painterResource(id = currentFlag),
+                    contentDescription = "Player Flag",
+                    modifier = Modifier.size(48.dp)
+                )
+
+                // Gems
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = gemImageResourceId),
+                        contentDescription = "Gems",
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    val currentGemCount by gemCount.collectAsState(initial = 0)
+                    Text(
+                        text = currentGemCount.toString(),
+                        color = Color.White,
+                        style = TextStyle(fontWeight = FontWeight.Medium, fontSize = 16.sp)
+                    )
+                }
+
+                // Energy
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = energyImageResourceId),
+                        contentDescription = "Energy",
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    val currentEnergyCount by energyCount.collectAsState(initial = 0)
+                    Text(
+                        text = currentEnergyCount.toString(),
+                        color = Color.White,
+                        style = TextStyle(fontWeight = FontWeight.Medium, fontSize = 16.sp)
+                    )
+                }
+            }
+        },
+        modifier = Modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = Color(0xFF00A2EA)
+        )
+    )
+    HorizontalDivider(thickness = bottomBorderSize, color = bottomBorderColor)
+}
