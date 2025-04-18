f94d9895dc1795bb3badcd7698d5b9f5c52d699c
Fhanafii <fhanafi544@gmail.com>
2025-04-18 10:16:40 +0700

    Creating Shop Fragment layout

--- app/src/main/java/com/fhanafi/cerdikia/ui/shop/ShopFragment.kt
+++ app/src/main/java/com/fhanafi/cerdikia/ui/shop/ShopFragment.kt
@@ -6,18 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.fhanafi.cerdikia.R
 import com.fhanafi.cerdikia.databinding.FragmentShopBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+
 
 class ShopFragment : Fragment() {
 
     private var _binding: FragmentShopBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -25,16 +25,37 @@ class ShopFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val shopViewModel =
-            ViewModelProvider(this).get(ShopViewModel::class.java)
 
         _binding = FragmentShopBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        // In your Fragment/Adapter
+        // Get a reference to the RecyclerView from the binding
+        val recyclerViewMisi = binding.recyclerViewMisi
+        recyclerViewMisi.layoutManager = LinearLayoutManager(requireContext())
 
-        val textView: TextView = binding.textNotifications
-        shopViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val recycleViewToko = binding.recyclerViewToko
+        recycleViewToko.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        val tokoItems = listOf(
+            Toko(R.drawable.img_pencil, 1000),
+            Toko(R.drawable.img_pencil, 1000),
+            Toko(R.drawable.img_pencil, 1000),
+            Toko(R.drawable.img_pencil, 1000),
+            // Add more toko items here if needed
+        )
+
+        // Sample data for Misi Harian
+        val misiList = listOf(
+            MisiHarian(R.drawable.ic_coin, "Dapatkan 50 XP", 45, 50),
+            // Add more misi items here if needed
+        )
+
+        val adapterToko = TokoAdapter(tokoItems)
+        recycleViewToko.adapter = adapterToko
+
+        val adapterMisi = MisiHarianAdapter(misiList)
+        recyclerViewMisi.adapter = adapterMisi
+
         onBackButtonPressed()
         return root
     }
